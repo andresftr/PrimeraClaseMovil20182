2daef04b8059601a9643e8cee957241002dfb0ce
Andres Tarra <afelipe0113@gmail.com>
2018-08-16 20:36:52 -0500

    Funcionalidad sin validaciones

---
+++ app/src/main/java/com/holamundo/calculadora/Metodos.java
@@ -0,0 +1,19 @@
+package com.holamundo.calculadora;
+
+public class Metodos {
+    public static double sumar(double n1, double n2){
+        return n1+n2;
+    }
+
+    public static double restar(double n1, double n2){
+        return n1-n2;
+    }
+
+    public static double multiplicar(double n1, double n2){
+        return n1*n2;
+    }
+
+    public static double dividir(double n1, double n2){
+        return n1/n2;
+    }
+}
